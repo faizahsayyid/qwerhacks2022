879e01051e8ba639b81f948e5495ae7e365a2118
faizahsayyid <faizah.sayyid@gmail.com>
2022-01-22 15:49:34 -0500

    added config

--- airtable.js
+++ airtable.js
@@ -1,7 +1,6 @@
 import Airtable from "airtable";
+import config from "./config.json";
 
-const base = new Airtable({ apiKey: "keyOAIfOrMiHLLpsx" }).base(
-  "appYYtplUj9bJWROv"
-);
+const base = new Airtable({ apiKey: config.userKey }).base(config.baseKey);
 
 export default base;
